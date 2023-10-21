21c696dd1595933ceba947431de02954e26336e5
Giorgio Grassini <giorgiograssini@gmail.com>
2023-10-21 17:02:57 +0200

    ready

--- js/Dice.js
+++ js/Dice.js
@@ -1,9 +1,1 @@
-class Dice {
-  constructor() {
-    this.value = 1;
-  }
-
-  roll() {
-    this.value = Math.floor(Math.random() * 6) + 1;
-  }
-}
+class Dice {}
--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,1 @@
-class BattleGame {
-  constructor(dice, player, enemy) {
-    this.dice = dice;
-    this.player = player;
-    this.enemy = enemy;
-  }
-
-  battle(attacker, defender) {
-    this.dice.roll();
-    attacker.attackPlayer(defender, this.dice.value);
-  }
-}
+class BattleGame {}
