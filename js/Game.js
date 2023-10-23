97750068c165938c6b75dbe09748b10a659bfdea
Cristina <c.gomezlop95@gmail.com>
2023-10-23 21:06:09 +0200

    first exercises completed

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,9 @@
-class BattleGame {}
+class BattleGame {
+  constructor(dice, player, enemy) {
+    this.dice = dice;
+    this.player = player;
+    this.enemy = enemy;
+  }
+
+  battle(attacker, defender) {}
+}
--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,25 @@
-class Player {}
+class Player {
+  constructor(name, life, attack) {
+    this.name = name;
+    this.life = life;
+    this.attack = attack;
+  }
+
+  attackPlayer(player, diceValue) {
+    if (this.life > 0) {
+      player.life -= this.attack * diceValue;
+    }
+
+    if (this.life <= 0) {
+      player.life += 0;
+    }
+  }
+
+  isDead() {
+    if (this.life <= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
