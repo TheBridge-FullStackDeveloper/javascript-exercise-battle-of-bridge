a308720329e8a049fee2ef33103c6d66d1a7722b
Mimimbres <miguel.nnez@gmail.com>
2023-10-23 21:25:10 +0200

    Casi funcional, falta manejar vida a 0 y que ataque :/

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,17 @@
-class Player {}
+class Player {
+    constructor (name, life, attack){
+    this.name = name;
+    this.life = life;
+    this.attack = attack;
+    }
+    attackPlayer(player, diceValue){
+        player.life -= diceValue.value * this.attack
+    }
+    isDead(){
+        if (this.life <= 0){
+            return true
+        }else{
+            return false
+        }
+    }
+}
