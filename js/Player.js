ccfe4c65636ce1688337dcc5a5be6abce997476b
PMFrancisco <francisco.perez.munoz@gmail.com>
2023-10-24 19:02:49 +0200

    Pasa todo Jasmine

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,16 @@
-class Player {}
+class Player {
+  constructor(name, life, attack) {
+    this.name = name;
+    this.life = life;
+    this.attack = attack;
+  }
+  attackPlayer(player, diceValue) {
+    if (this.isDead()) {
+      return;
+    }
+    player.life -= this.attack * diceValue;
+  }
+  isDead() {
+    return this.life <= 0;
+  }
+}
