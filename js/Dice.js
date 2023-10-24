fab01545cf8b2ce7c7897a049f58c5981c1b2e7e
Manuk <141228609+manuelskoppe@users.noreply.github.com>
2023-10-24 14:11:38 +0200

    ejercicio//completado//funcionamiento//ok

--- js/Dice.js
+++ js/Dice.js
@@ -1,1 +1,10 @@
-class Dice {}
+class Dice {
+    constructor() {
+        this.value = 1; 
+    }
+
+    roll() {
+        this.value = Math.floor(Math.random() * 6) + 1;
+        return this.value;
+    }
+}
--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,12 @@
-class BattleGame {}
+class BattleGame {
+    constructor(dice, player, enemy) {
+        this.dice = dice;
+        this.player = player;
+        this.enemy = enemy;
+    }
+
+    battle(attacker, defender) {
+        let diceValue = this.dice.roll();
+        attacker.attackPlayer(defender, diceValue);
+    }
+}
