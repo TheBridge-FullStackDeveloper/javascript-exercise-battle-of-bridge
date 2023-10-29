ee4605636f824945d06dc1588818d7284d511407
Mimimbres <miguel.nnez@gmail.com>
2023-10-29 17:40:33 +0100

    todo verde. falta bonus

--- js/Player.js
+++ js/Player.js
@@ -1,17 +1,17 @@
 class Player {
-    constructor (name, life, attack){
+  constructor(name, life, attack) {
     this.name = name;
     this.life = life;
     this.attack = attack;
+  }
+  attackPlayer(player, diceValue) {
+    if (this.isDead() === true) {
+      return;
+    } else {
+      player.life -= this.attack * diceValue;
     }
-    attackPlayer(player, diceValue){
-        player.life -= diceValue.value * this.attack
-    }
-    isDead(){
-        if (this.life <= 0){
-            return true
-        }else{
-            return false
-        }
-    }
+  }
+  isDead() {
+    return this.life <= 0;
+  }
 }
